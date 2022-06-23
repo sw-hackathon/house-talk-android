9de4f6364bd3c4538e116a13ac201e5d3ce1571f
xxeol2 <jhj01177@naver.com>
2022-06-24 00:08:21 +0900

    [FEAT] Search 기능 구현

---
+++ app/src/main/java/com/likefirst/meyouhouse/data/remote/search/ReviewData.kt
@@ -0,0 +1,9 @@
+package com.likefirst.meyouhouse.data.remote.search
+
+data class ReviewData(
+    val name: String,
+    val address: String,
+    val point: Int,
+    val heart: Int,
+    val answer: Int
+)
